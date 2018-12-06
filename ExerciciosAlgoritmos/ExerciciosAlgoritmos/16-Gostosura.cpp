4d140f21f9dfbf17086b0400adc538af83ff33ef
Ygor Vieira <ygorus@gmail.com>
2018-12-06 18:53:53 -0200

    Exercicio 16 corrigido

--- ExerciciosAlgoritmos/ExerciciosAlgoritmos/16-Gostosura.cpp
+++ ExerciciosAlgoritmos/ExerciciosAlgoritmos/16-Gostosura.cpp
@@ -4,16 +4,17 @@ using namespace std;
 
 int main() {
 	int quantidade;
-	float queijo, presunto;
+	float queijo, presunto, carne;
 
 	cout << "Informe a quantidade de sanduiches: " << endl;
 	cin >> quantidade;
 
-	queijo = (100 * quantidade) / 1000.00;
+	queijo = (50 * quantidade) / 1000.00;
 	presunto = (50 * quantidade) / 1000.00;
+	carne = (100 * quantidade) / 1000.00;
 
-	cout << "Serao necessarios " << queijo << "Kg de queijo e " << presunto 
-			<< "Kg de presunto." << endl;
+	cout << "Serao necessarios " << queijo << "Kg de queijo, " << presunto 
+			<< "Kg de presunto e " << carne << "Kg de carne para fazer os sanduiches." << endl;
 
 	system("PAUSE");
 	return 0;
